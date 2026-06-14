41145de25028857604c93c5b0d6259a070572044
MarkusSaw <vadirai@yandex.ru>
2026-06-14 22:55:02 +0300

    feature(Spring-1): Добавлены связи в таблицу students , изменён путь db.changelog.yaml в application.yaml.

--- src/main/resources/liquibase/changesets/03_create_table_students.sql
+++ src/main/resources/liquibase/changesets/03_create_table_students.sql
@@ -4,6 +4,15 @@ CREATE TABLE students (
       id BIGINT NOT NULL AUTO_INCREMENT,
       name VARCHAR(255),
       lastname VARCHAR(255),
-      group_name VARCHAR(255)
+      group_name VARCHAR(255),
+      course_id BIGINT NOT NULL,
+      group_id BIGINT NOT NULL,
+      teacher_id BIGINT NOT NULL,
+      timesheet_id BIGINT NOT NULL,
       PRIMARY KEY (id)
+
+      FOREIGN KEY (course_id) REFERENCES course(id),
+      FOREIGN KEY (group_id) REFERENCES groups(id),
+      FOREIGN KEY (teacher_id) REFERENCES teacher(id),
+      FOREIGN KEY (timesheet_id) REFERENCES timesheet(id)
 );
